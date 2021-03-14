e929c2cf603d2165b8c9bbaa14d0898514fdb9d6
Muhammad Bilal <hafizbali315@gmail.com>
2021-03-14 18:16:23 +0500

    #14 status(2)

--- client/src/components/StatusModal.js
+++ client/src/components/StatusModal.js
@@ -1,12 +1,81 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { GLOBALTYPES } from '../redux/constants'
 
 const StatusModal = () => {
-	const { auth } = useSelector((state) => state)
+	const { auth, theme } = useSelector((state) => state)
 	const dispatch = useDispatch()
 
 	const [content, setContent] = useState('')
+	const [images, setImages] = useState([])
+
+	const [stream, setStream] = useState(false)
+	const videoRef = useRef()
+	const refCanvas = useRef()
+	const [tracks, setTracks] = useState('')
+
+	const handleChangeImages = (e) => {
+		const files = [...e.target.files]
+		let error = ''
+		let newImages = []
+
+		files.forEach((file) => {
+			if (!file) return (error = 'Please select a file.')
+
+			if (file.type !== 'image/jpeg' && file.type !== 'image/png') return (error = 'Image format is incorrect.')
+
+			return newImages.push(file)
+		})
+
+		if (error) dispatch({ type: GLOBALTYPES.ALERT, payload: { error: error } })
+
+		setImages([...images, ...newImages])
+	}
+
+	const deleteImages = (index) => {
+		const newArr = [...images]
+		newArr.splice(index, 1)
+		setImages(newArr)
+	}
+
+	const handleStream = () => {
+		setStream(true)
+
+		if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+			navigator.mediaDevices
+				.getUserMedia({ video: true })
+				.then((mediaStream) => {
+					videoRef.current.srcObject = mediaStream
+					videoRef.current.play()
+
+					const track = mediaStream.getTracks()
+
+					setTracks(track[0])
+				})
+				.catch((error) => {
+					console.log(error)
+				})
+		}
+	}
+
+	const handleCapture = () => {
+		const width = videoRef.current.clientWidth
+		const height = videoRef.current.clientHeight
+
+		refCanvas.current.setAttribute('width', width)
+		refCanvas.current.setAttribute('height', height)
+
+		const ctx = refCanvas.current.getContext('2d')
+		ctx.drawImage(videoRef.current, 0, 0, width, height)
+		let URL = refCanvas.current.toDataURL()
+
+		setImages([...images, { camera: URL }])
+	}
+
+	const handleStopStream = () => {
+		tracks.stop()
+		setStream(false)
+	}
 
 	return (
 		<div className="status_modal">
@@ -24,14 +93,42 @@ const StatusModal = () => {
 						onChange={(e) => setContent(e.target.value)}
 					/>
 
-					<div className="input_images">
-						<i className="fas fa-camera" />
+					<div className="show_images">
+						{images.map((img, index) => (
+							<div key={index} id="file_img">
+								<img
+									src={img.camera ? img.camera : URL.createObjectURL(img)}
+									alt="images"
+									className="img-thumbnail"
+									style={{ filter: theme ? 'invert(1)' : 'invert(0)' }}
+								/>
+								<span onClick={() => deleteImages(index)}>&times;</span>
+							</div>
+						))}
+					</div>
 
-						<div className="file_upload">
-							<i className="fas fa-images" />
+					{stream && (
+						<div className="stream position-relative">
+							<video autoPlay muted ref={videoRef} width="100%" height="100%" style={{ filter: theme ? 'invert(1)' : 'invert(0)' }} />
 
-							<input type="file" name="file" id="file" multiple accept="image/*" />
+							<span onClick={handleStopStream}>&times;</span>
+							<canvas ref={refCanvas} style={{ display: 'none' }} />
 						</div>
+					)}
+
+					<div className="input_images">
+						{stream ? (
+							<i className="fas fa-camera" onClick={handleCapture} />
+						) : (
+							<>
+								<i className="fas fa-camera" onClick={handleStream} />
+								<div className="file_upload">
+									<i className="fas fa-images" />
+
+									<input type="file" name="file" id="file" multiple accept="image/*" onChange={handleChangeImages} />
+								</div>
+							</>
+						)}
 					</div>
 				</div>
 
